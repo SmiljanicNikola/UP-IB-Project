1255fdea9413b6776955e8e8e7c98bc8279f5a3a
Nikola Smiljanic <nikola.kiki.00@gmail.com>
2021-05-08 15:01:34 +0200

    proba2

--- UP-IBProjekat/src/main/java/com/example/UPIBProjekat/model/Pregled.java
+++ UP-IBProjekat/src/main/java/com/example/UPIBProjekat/model/Pregled.java
@@ -7,9 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.print.attribute.standard.DateTimeAtCompleted;
 
-@Entity
+@Table(name = "pregled")
+@Entity(name="Pregled")
 public class Pregled {
 
 	private int id;
